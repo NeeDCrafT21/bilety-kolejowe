8e322133c123bb732117c42390059a1a41351ad6
RafalLisiewicz <rafal.lisiewicz@student.wat.edu.pl>
2023-01-22 21:31:25 +0100

    Adding Time tables

--- BackEndApplication/Program.cs
+++ BackEndApplication/Program.cs
@@ -1,3 +1,26 @@
-﻿// Gówno dupa
+﻿using System.Collections;
+using BackEndApplication;
 
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //filling time tables
+        TimeTable A = new TimeTable();
+        TimeTable B = new TimeTable();
+        TimeTable C = new TimeTable();
+
+        A.fillTable('A');
+        B.fillTable('B');
+        C.fillTable('C');
+        
+        //TODO dane przesłane
+        TimeOnly startTime = new TimeOnly(8,0);
+        char departurePlace = 'A';
+        char arrivalPlace = 'B';
+        //koniec przesłanych danych
+        
+        
+    }
+}
---
+++ BackEndApplication/Ride.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace BackEndApplication;
+
+public class Ride
+{
+    private char destination;
+    private TimeOnly hour;
+    private TimeSpan duration;
+
+    public Ride(char destination, TimeOnly hour, TimeSpan duration)
+    {
+        this.destination = destination;
+        this.hour = hour;
+        this.duration = duration;
+    }
+
+    public char getDestination()
+    {
+        return destination;
+    }
+    public TimeOnly getHour()
+    {
+        return hour;
+    }
+
+    public TimeSpan getDuration()
+    {
+        return duration;
+    }
+}
