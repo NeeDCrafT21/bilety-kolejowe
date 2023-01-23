1788731617907d660c3d238228729b78f51c5ef1
RafalLisiewicz <rafal.lisiewicz@student.wat.edu.pl>
2023-01-23 23:20:03 +0100

    Added a lot of functionality (scorecard) and Ticket.cs

---
+++ BackEndApplication/PreTimeTable.cs
@@ -0,0 +1,28 @@
+﻿namespace BackEndApplication;
+
+public abstract class PreTimeTable
+{
+    protected char cityName;
+
+    protected PreTimeTable(char name)
+    {
+        city = name;
+    }
+    public char city
+    {
+        get
+        {
+            return cityName;
+        }
+
+        set
+        {
+            cityName = value;
+        }
+    }
+
+    public abstract void setTable(Task<List<Ride<TimeOnly>>> rides);
+    public abstract void PrintTable();
+    public abstract Task<List<Ride<TimeOnly>>> generateTable();
+
+}
