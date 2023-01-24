182fd9395c63be766a71c053e22a508332beee24
RafalLisiewicz <rafal.lisiewicz@student.wat.edu.pl>
2023-01-25 00:18:30 +0100

    Implemented rout finding

---
+++ BackEndApplication/Calculator.cs
@@ -0,0 +1,86 @@
+﻿namespace BackEndApplication;
+
+public class Calculator
+{
+    private MessageTicket ticket;
+    private List<TimeTable> tables = new List<TimeTable>();
+    private List<AbstractTicket> output;
+
+    public Calculator(MessageTicket ticket, params TimeTable[] tables)
+    {
+        this.ticket = ticket;
+        foreach (TimeTable table in tables)
+        {
+            this.tables.Add(table);
+        }
+    }
+
+    public void Print()
+    {
+        output[0].Print();
+        output[1].Print();
+    }
+    public List<AbstractTicket> FindRoute()
+    {
+        int i = tables.FindIndex(a => a.GetCity() == ticket.GetDeparturePlace());
+        int j = tables.FindIndex(a => a.GetCity() != ticket.GetArrivalPlace() && a.GetCity() != ticket.GetDeparturePlace());
+        
+        //find closest ride without train change
+        Ride<TimeOnly> ride1 = tables[i].GetRides().SkipWhile(p=> 
+            p.getHour() <= ticket.GetTime() || p.getDestination() == ticket.GetArrivalPlace()).FirstOrDefault();
+        if(ride1 == null)
+        {
+            if (tables[i].GetRides()[0].getDestination() == ticket.GetArrivalPlace())
+            {
+                ride1 = tables[i].GetRides()[0];
+            }
+            else
+            {
+                ride1 = tables[i].GetRides()[2];
+            }
+        }
+
+        Ticket ticket1 = new Ticket(ride1.getHour(), ticket.GetDeparturePlace(), ride1.getDestination(), ride1.getDuration());
+
+        Ride<TimeOnly> ride2 = tables[i].GetRides().SkipWhile(p =>
+            p.getHour() <= ticket.GetTime() || p.getDestination() == tables[j].GetCity()).FirstOrDefault();
+        if (ride2 == null)
+        {
+            if (tables[i].GetRides()[0].getDestination() == tables[j].GetCity())
+            {
+                ride2 = tables[i].GetRides()[0];
+            }
+            else
+            {
+                ride2 = tables[i].GetRides()[2];
+            }
+        }
+
+        int x = 0;
+        Ride<TimeOnly> ride3 = tables[j].GetRides().SkipWhile(p=> 
+            p.getHour() <= ticket.GetTime().Add(ride2.getDuration(), out x) || p.getDestination() == ticket.GetArrivalPlace()).FirstOrDefault();
+        
+        if (x!=0 || ride3 == null)
+        {
+            if (tables[j].GetRides()[0].getDestination() == ticket.GetArrivalPlace())
+            {
+                ride3 = tables[j].GetRides()[0];
+            }
+            else
+            {
+                ride3 = tables[j].GetRides()[2];
+            }
+        }
+        
+        List<AbstractTicket> temp = new List<AbstractTicket>();
+        AbstractTicket temp1 = new Ticket(ride1.getHour(),ticket.GetDeparturePlace(),ticket.GetArrivalPlace(),ride1.getDuration());
+        AbstractTicket temp2 = new TicketAdvanced(ride2.getHour(),ticket.GetDeparturePlace(),ride2.getDestination(),ride2.getDuration(),ride3.getHour(),tables[j].GetCity(),ticket.GetArrivalPlace(), ride3.getDuration());
+        
+        
+        temp.Add(temp1);
+        temp.Add(temp2);
+        output = temp;
+        
+        return temp;
+    }
+}
---
+++ BackEndApplication/TicketAdvanced.cs
@@ -0,0 +1,50 @@
+﻿namespace BackEndApplication;
+
+public class TicketAdvanced : AbstractTicket
+{
+    private TimeOnly startTime2;
+    private char departurePlace2;
+    private char arrivalPlace2;
+    private TimeSpan partialDuration;
+    private TimeSpan duration;
+    
+    public TicketAdvanced(TimeOnly startTime, char departurePlace, char arrivalPlace, TimeSpan partialDuration, TimeOnly startTime2,char departurePlace2, char arrivalPlace2, TimeSpan duration )
+    {
+        this.startTime = startTime;
+        this.departurePlace = departurePlace;
+        this.arrivalPlace = arrivalPlace;
+        this.partialDuration = partialDuration;
+        
+        this.startTime2 = startTime;
+        this.departurePlace2 = departurePlace2;
+        this.arrivalPlace2 = arrivalPlace2;
+        
+        this.duration = (this.startTime2 - this.startTime) + duration;
+    }
+
+    public TimeOnly GetTime2()
+    {
+        return startTime2;
+    }
+    public char GetArrivalPlace2()
+    {
+        return arrivalPlace2;
+    }
+    
+    public char GetDeparturePlace2()
+    {
+        return departurePlace2;
+    }
+    
+    public TimeSpan GetDuration()
+    {
+        return duration;
+    }
+    
+    public override void Print()
+    {
+        Console.WriteLine("\nBilet2");
+        Console.WriteLine("Przed przesiadką: "+startTime+" "+ departurePlace+" "+arrivalPlace+" "+partialDuration+".");
+        Console.WriteLine("Po przesiadce: "+startTime2+" "+ departurePlace2+" "+arrivalPlace2+" "+duration+".");
+    }
+}
