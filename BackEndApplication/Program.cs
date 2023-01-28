4cb229067013d2f5460ccc0737f1b0704cd27222
RafalLisiewicz <rafal.lisiewicz@student.wat.edu.pl>
2023-01-28 18:42:29 +0100

    kosmetyczne poprawki

--- BackEndApplication/Program.cs
+++ BackEndApplication/Program.cs
@@ -13,37 +13,36 @@ class Program
         StreamWriter writer = new StreamWriter(server);
         
         var message = reader.ReadLine();
+        
+        TimeTable A = new TimeTable('A');
+        TimeTable B = new TimeTable('B');
+        TimeTable C = new TimeTable('C');
 
-        //filling time tables
-            TimeTable A = new TimeTable('A');
-            TimeTable B = new TimeTable('B');
-            TimeTable C = new TimeTable('C');
-
-            var (taskA, taskB, taskC) = (A.GenerateTable(), B.GenerateTable(), C.GenerateTable());
-            var tempA = (async () => await taskA);
-            A.SetTable(tempA());
-            var tempB = (async () => await taskB);
-            B.SetTable(tempB());
-            var tempC = (async () => await taskC);
-            C.SetTable(tempC());
+        var (taskA, taskB, taskC) = (A.GenerateTable(), B.GenerateTable(), C.GenerateTable());
+        var tempA = (async () => await taskA);
+        A.SetTable(tempA());
+        var tempB = (async () => await taskB);
+        B.SetTable(tempB());
+        var tempC = (async () => await taskC);
+        C.SetTable(tempC());
             
 
-            MessageTicket ticket = JsonSerializer.Deserialize<MessageTicket>(message);
+        MessageTicket ticket = JsonSerializer.Deserialize<MessageTicket>(message);
             
-            ticket.Print();
+        ticket.Print();
 
-            Calculator mill = new Calculator(ticket, A, B, C);
+        Calculator mill = new Calculator(ticket, A, B, C);
 
-            MessageTicket2 tickets = mill.FindRoute();
+        MessageTicket2 tickets = mill.FindRoute();
 
-            tickets.Print();
+        tickets.Print();
 
-            string message2 = JsonSerializer.Serialize(tickets);
+        string message2 = JsonSerializer.Serialize(tickets);
 
-            Console.WriteLine("Wiadomość: "+message2);
+        Console.WriteLine("Wiadomość: "+message2);
 
-            writer.WriteLine(message2);
-            writer.Flush();
+        writer.WriteLine(message2);
+        writer.Flush();
 
     }
 }
